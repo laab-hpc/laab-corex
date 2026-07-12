f8c6b170163420a6142e2e34f5118f8dc18dedca
Aravind Sankaran <aravind.sankaran@rwth-aachen.de>
2026-07-12 14:20:03 +0200

    removed pandas dep in ranking. Added interface to profile keys

--- src/benchmark_codes/dense/GEMM/cpu/distributed/scalapack/test/submit.keb.sh
+++ src/benchmark_codes/dense/GEMM/cpu/distributed/scalapack/test/submit.keb.sh
@@ -2,15 +2,22 @@
 #SBATCH -A hpc2n2026-184
 #SBATCH --job-name=laab-pdgemm
 #SBATCH --nodes=2
-#SBATCH --ntasks-per-node=16
-#SBATCH --cpus-per-task=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --cpus-per-task=14
 #SBATCH --time=00:20:00
 #SBATCH --output=slurm.out
 #SBATCH --error=slurm.err
+#SBATCH -C skylake
 
 set -euo pipefail
 
 export LAAB_BUILD_DIR=$(pwd)/build
+export LAAB_TRACE_DIR=$(pwd)/traces
+mkdir -p $LAAB_TRACE_DIR
+
+GIT_ROOT=$(git rev-parse --show-toplevel)
+INPUTS_DIR=$GIT_ROOT/inputs
+
 
 module load GCC OpenMPI ScaLAPACK
 
@@ -21,8 +28,5 @@ make -C ../ clean
 make -C ../
 
 
-MATRIX_DIR=../practise/matrices/dense
-export LAAB_TRACE_DIR=$(pwd)/traces
-mkdir -p $LAAB_TRACE_DIR
-
-srun -N 2 -n 16 ./build/pdgemm.exe -A $MATRIX_DIR/M15000x15000-float64-gen.dense -B $MATRIX_DIR/M15000x15000-float64-gen.dense -b 256 --reps 5
+export OMP_NUM_THREADS=14
+srun -N 2 -n 4 -c 14 ./build/pdgemm.exe -A $INPUTS_DIR/dense/M15000x15000-float64-gen.dense -B $INPUTS_DIR/dense/M15000x15000-float64-gen.dense -b 256 --reps 5
