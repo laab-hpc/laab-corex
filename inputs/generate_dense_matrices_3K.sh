334b7143d5d1b8de0dc54384d0b2ce5e370d40f8
Aravind Sankaran <aravind.sankaran@rwth-aachen.de>
2026-08-23 22:49:09 +0200

    Added keb experiment new checkpoints.

---
+++ inputs/generate_dense_matrices_3K.sh
@@ -0,0 +1,3 @@
+mgen --outdir dense/ --prec float64 --shape 3000x3000 --workers 12
+mgen --outdir dense/ --prec float32 --shape 3000x3000 --workers 12
+mgen --outdir dense/ --prec complex128 --shape 3000x3000 --workers 12
